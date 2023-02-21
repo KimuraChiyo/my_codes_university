2a18a23b73840b5658d7581395dabf1b41927002
Leonid Solovyev <dan_backyard07@mail.ru>
2023-02-21 21:09:34 +0300

    QSort

---
+++ Grokking Algorithms/QSort_recursion.py
@@ -0,0 +1,10 @@
+def qsort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        supp_elem = arr[0]
+        less_arr = [i for i in arr[1:] if i < supp_elem]
+        greater_arr = [i for i in arr[1:] if i >= supp_elem]
+        return qsort(less_arr) + [supp_elem] + qsort(greater_arr)
+
+print(qsort([100,123,1,23,4,15,16,2,3,1,23,4,5123,1423,3234,12345,100000]))
