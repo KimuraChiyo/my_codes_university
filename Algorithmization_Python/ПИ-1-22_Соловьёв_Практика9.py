841c20a73ec8c12e5054d75c42c8243cb4625a04
Leonid Solovyev <dan_backyard07@mail.ru>
2023-04-11 16:11:26 +0300

    upd

--- Algorithmization_Python/ПИ-1-22_Соловьёв_Практика9.py
+++ Algorithmization_Python/ПИ-1-22_Соловьёв_Практика9.py
@@ -42,14 +42,14 @@ for i in range(n):
 matrix = transposed(matrix)
 [print(*row, sep='\t') for row in matrix]
 print()
-
+'''
 # 4
 print("4. Дана матрица размера M × N и целое число K (1 ≤ K ≤ M). Вывести элементы K-й строки данной матрицы.")
 m, n = int(input("Введите m: ")), int(input("Введите n: "))
 k = int(input("Введите k: ")) - 1
 matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
 [print(*row, sep='\t') for row in matrix]
-print(str(k + 1) + " строка:", *matrix[k], sep='\t')
+print(str(k + 1) + " строка:", *matrix[k], sep='\t', end='\n\n')
 
 # 5
 print("5. Дана матрица размера M × N и целое число K (1 ≤ K ≤ N). Вывести элементы K-го столбца данной матрицы.")
@@ -57,7 +57,7 @@ m, n = int(input("Введите m: ")), int(input("Введите n: "))
 k = int(input("Введите k: ")) - 1
 matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
 [print(*row, sep='\t') for row in matrix]
-print(str(k + 1) + " столбец:", *[matrix[i][k] for i in range(len(matrix))], sep='\t')
+print(str(k + 1) + " столбец:", *[matrix[i][k] for i in range(len(matrix))], sep='\t', end='\n'*2)
 
 # 6
 print("6. Дана матрица размера M × N. Вывести ее элементы, расположенныев строках с четными номерами (2, 4, …). Вывод элементов производить по строкам, условный оператор не использовать.")
@@ -68,7 +68,7 @@ print()
 [print(*matrix[row], sep='\t') for row in range(1, len(matrix), 2)]
 
 # 7
-print("7. Дана матрица размера M × N. Вывести ее элементы, расположенныев столбцах с нечетными номерами (1, 3, …). Вывод элементов производить по столбцам, условный оператор не использовать.")
+print("\n7. Дана матрица размера M × N. Вывести ее элементы, расположенныев столбцах с нечетными номерами (1, 3, …). Вывод элементов производить по столбцам, условный оператор не использовать.")
 m, n = int(input("Введите m: ")), int(input("Введите n: "))
 matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
 [print(*row, sep='\t') for row in matrix]
@@ -76,91 +76,6 @@ print()
 [print(*[row[i - 1] for i in range(1, len(row) + 1, 2)], sep='\t') for row in matrix]
 
 # 8
-print("8. Дана матрица размера M × N и целое число K (1 ≤ K ≤ M). Найтисумму и произведение элементов K-й строки данной матрицы.")
-m, n = int(input("Введите m: ")), int(input("Введите n: "))
-k = int(input("Введите k: "))
-matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
-[print(*row, sep='\t') for row in matrix]
-print()
-print("Сумма:", sum(matrix[k - 1]), "\nПроизведение:", prod(matrix[k - 1]))
-
-# 9
-print("9. Дана матрица размера M × N и целое число K (1 ≤ K ≤ N). Найтисумму и произведение элементов K-го столбца данной матрицы.")
-m, n = int(input("Введите m: ")), int(input("Введите n: "))
-k = int(input("Введите k: ")) - 1
-matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
-[print(*row, sep='\t') for row in matrix]
-print()
-print("Сумма:", sum([matrix[i][k] for i in range(len(matrix))]), "\nПроизведение:", prod([matrix[i][k] for i in range(len(matrix))]))
-
-# 10
-print("10. Дана матрица размера M × N. В каждой строке матрицы найти минимальный элемент.")
-m, n = int(input("Введите m: ")), int(input("Введите n: "))
-matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
-[print(*row, sep='\t') for row in matrix]
-print()
-[print("Минимум строки " + str(i + 1) + ":", min(matrix[i])) for i in range(len(matrix))]
-
-# 11
-print("11. Дана матрица размера M × N. В каждом столбце матрицы найти максимальный элемент.")
-m, n = int(input("Введите m: ")), int(input("Введите n: "))
-matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
-[print(*row, sep='\t') for row in matrix]
-print()
-[print(10*' ' + "Максимум столбца " + str(i + 1) + ":", max([matrix[k][i] for k in range(len(matrix))])) for i in range(len(matrix[1]))]
-
-# 12
-print("12. Дана матрица размера M × N. Найти максимальный среди минимальных элементов ее строк.")
-m, n = int(input("Введите m: ")), int(input("Введите n: "))
-matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
-print()
-print("Максимум минимумов:",  max([min(matrix[i]) for i in range(len(matrix))]))
-
-# 13
-print("13. Дана матрица размера M × N и целые числа K1 и K2 (1 ≤ K1 < K2 ≤ M). Поменять местами строки матрицы с номерами K1 и K2.")
-m, n = int(input("Введите m: ")), int(input("Введите n: "))
-k, l = int(input("Введите k: ")) - 1, int(input("Введите l: ")) - 1
-matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
-[print(*row, sep='\t') for row in matrix]
-print()
-matrix[k], matrix[l] =matrix[l], matrix[k]
-[print(*row, sep='\t') for row in matrix]
-
-# 14
-print("14. Дана матрица размера M × N и целые числа K1 и K2 (1 ≤ K1 < K2 ≤ N). Поменять местами столбцы матрицы с номерами K1 и K2.")
-m, n = int(input("Введите m: ")), int(input("Введите n: "))
-k, l = int(input("Введите k: ")) - 1, int(input("Введите l: ")) - 1
-matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
-[print(*row, sep='\t') for row in matrix]
-print()
-for i in range(len(matrix)):
-    matrix[i][k], matrix[i][l] =matrix[i][l], matrix[i][k]
-[print(*row, sep='\t') for row in matrix]
-
-# 15
-print("15. Дана матрица размера M × N. Преобразовать матрицу, поменяв местами минимальный и максимальный элемент в каждой строке.")
-m, n = int(input("Введите m: ")), int(input("Введите n: "))
-matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
-[print(*row, sep='\t') for row in matrix]
-print()
-for i in range(len(matrix)):
-    mx_ind, mn_ind = matrix[i].index(max(matrix[i])), matrix[i].index(min(matrix[i]))
-    matrix[i][mx_ind], matrix[i][mn_ind] = matrix[i][mn_ind], matrix[i][mx_ind]
-[print(*row, sep='\t') for row in matrix]
-
-# 16
-print("16. Дана матрица размера M × N. Преобразовать матрицу, поменяв местами минимальный и максимальный элемент в каждом столбце.")
-m, n = int(input("Введите m: ")), int(input("Введите n: "))
-matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
-[print(*row, sep='\t') for row in matrix]
-print()
-matrix = transposed(matrix)
-for i in range(len(matrix)):
-    mx_ind, mn_ind = matrix[i].index(max(matrix[i])), matrix[i].index(min(matrix[i]))
-    matrix[i][mx_ind], matrix[i][mn_ind] = matrix[i][mn_ind], matrix[i][mx_ind]
-matrix = transposed(matrix)
-[print(*row, sep='\t') for row in matrix]
-
 treug = [[1]]
 print("8. Треугольник Паскаля. Вывести на экран треугольник Паскаля из n строк. Придумать структуру данных для хранения треугольника Паскаля (например, стандартная матрица, что, однако, не экономно). Реализовать показ треугольника по данным из этой структуры.")
 n = int(input("Введите число строк: "))
@@ -171,6 +86,97 @@ while len(treug) != n:
         new.append(last[i] + last[i + 1])
     treug.append(new)
 [print(*row) for row in treug]
+
+# 9
+print("\n9. Дана матрица размера M × N и целое число K (1 ≤ K ≤ M). Найтисумму и произведение элементов K-й строки данной матрицы.")
+m, n = int(input("Введите m: ")), int(input("Введите n: "))
+k = int(input("Введите k: "))
+matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
+[print(*row, sep='\t') for row in matrix]
+print()
+print("Сумма:", sum(matrix[k - 1]), "\nПроизведение:", prod(matrix[k - 1]))
+
+# 10
+print("\n10. Дана матрица размера M × N и целое число K (1 ≤ K ≤ N). Найтисумму и произведение элементов K-го столбца данной матрицы.")
+m, n = int(input("Введите m: ")), int(input("Введите n: "))
+k = int(input("Введите k: ")) - 1
+matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
+[print(*row, sep='\t') for row in matrix]
+print()
+print("Сумма:", sum([matrix[i][k] for i in range(len(matrix))]), "\nПроизведение:", prod([matrix[i][k] for i in range(len(matrix))]))
+
+# 11
+print("\n11. Дана матрица размера M × N. В каждой строке матрицы найти минимальный элемент.")
+m, n = int(input("Введите m: ")), int(input("Введите n: "))
+matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
+[print(*row, sep='\t') for row in matrix]
+print()
+[print("Минимум строки " + str(i + 1) + ":", min(matrix[i])) for i in range(len(matrix))]
+
+# 12
+print("\n12. Дана матрица размера M × N. В каждом столбце матрицы найти максимальный элемент.")
+m, n = int(input("Введите m: ")), int(input("Введите n: "))
+matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
+[print(*row, sep='\t') for row in matrix]
+print()
+[print(10*' ' + "Максимум столбца " + str(i + 1) + ":", max([matrix[k][i] for k in range(len(matrix))])) for i in range(len(matrix[1]))]
+
+# 13
+print("\n13. Дана матрица размера M × N. Найти максимальный среди минимальных элементов ее строк.")
+m, n = int(input("Введите m: ")), int(input("Введите n: "))
+matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
+print()
+print("Максимум минимумов:",  max([min(matrix[i]) for i in range(len(matrix))]))
+
+# 14
+print("\n14. Дана матрица размера M × N и целые числа K1 и K2 (1 ≤ K1 < K2 ≤ M). Поменять местами строки матрицы с номерами K1 и K2.")
+m, n = int(input("Введите m: ")), int(input("Введите n: "))
+k, l = int(input("Введите k: ")) - 1, int(input("Введите l: ")) - 1
+matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
+[print(*row, sep='\t') for row in matrix]
+print()
+matrix[k], matrix[l] =matrix[l], matrix[k]
+[print(*row, sep='\t') for row in matrix]
+
+# 15
+print("\n15. Дана матрица размера M × N и целые числа K1 и K2 (1 ≤ K1 < K2 ≤ N). Поменять местами столбцы матрицы с номерами K1 и K2.")
+m, n = int(input("Введите m: ")), int(input("Введите n: "))
+k, l = int(input("Введите k: ")) - 1, int(input("Введите l: ")) - 1
+matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
+[print(*row, sep='\t') for row in matrix]
+print()
+for i in range(len(matrix)):
+    matrix[i][k], matrix[i][l] =matrix[i][l], matrix[i][k]
+[print(*row, sep='\t') for row in matrix]
+
+# 16
+print("\n16. Дана матрица размера M × N. Преобразовать матрицу, поменяв местами минимальный и максимальный элемент в каждой строке.")
+m, n = int(input("Введите m: ")), int(input("Введите n: "))
+matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
+[print(*row, sep='\t') for row in matrix]
+print()
+for i in range(len(matrix)):
+    mx_ind, mn_ind = matrix[i].index(max(matrix[i])), matrix[i].index(min(matrix[i]))
+    matrix[i][mx_ind], matrix[i][mn_ind] = matrix[i][mn_ind], matrix[i][mx_ind]
+[print(*row, sep='\t') for row in matrix]
+
+# 17
+print("\n17. Дана матрица размера M × N. Преобразовать матрицу, поменяв местами минимальный и максимальный элемент в каждом столбце.")
+m, n = int(input("Введите m: ")), int(input("Введите n: "))
+matrix = [[randint(1, 100) for j in range(1, n + 1)] for i in range(1, m + 1)]
+[print(*row, sep='\t') for row in matrix]
+print()
+matrix = transposed(matrix)
+for i in range(len(matrix)):
+    mx_ind, mn_ind = matrix[i].index(max(matrix[i])), matrix[i].index(min(matrix[i]))
+    matrix[i][mx_ind], matrix[i][mn_ind] = matrix[i][mn_ind], matrix[i][mx_ind]
+matrix = transposed(matrix)
+[print(*row, sep='\t') for row in matrix]
+
+# 18
+print("\n18.")
+
+'''
 
 
 m, n = int(input("Введите m: ")), int(input("Введите n: "))
@@ -239,4 +245,4 @@ for i in range(m - 1, 0, -1):
 for diag in diagons:
     # print(sum(diag))
     print(min(diag))
-''' 
+'''
